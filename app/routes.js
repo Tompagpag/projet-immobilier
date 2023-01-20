a23e86bed6b2c6d076c809e94cfec0e7d943d221
Tom Paganel <paganel.tom@gmail.com>
2023-01-20 15:18:22 +0100

    remove old verifyJWT

--- app/routes.js
+++ app/routes.js
@@ -11,39 +11,6 @@ import Cookies from "cookies";
 
 export default (app) => {
 
-  const verifyJWT  = (req, res, next) => {
-    let token = new Cookies(req,res).get('access_token');
-    // Si le cookie (access_token) n'existe pas
-    if (typeof token == 'undefined' || token == '') {
-      if (token == '') {
-        res.clearCookie("access_token");
-      }
-      return res.sendStatus(401);
-    }
-
-    // sinon on vérifie le jwt
-    jwt.verify(token, process.env.JWT_SECRET, (err, dataJwt) => {
-      // Erreur du JWT (n'est pas un JWT, a été modifié, est expiré)
-      if(err) {
-          res.clearCookie("access_token");
-          return res.sendStatus(403);
-        }
-
-        // A partir de là le JWT est valide on a plus qu'à vérifier les droits
-        // Si on est admin
-        // if(typeof dataJwt.roles != 'undefined' && dataJwt.roles[0] == 'admin') {
-        //     return res.send(`Admin ${dataJwt.firstname}`);
-        // }
-        // Gestion des droits
-        req.session.user = {
-          username: dataJwt.username,
-          id: dataJwt.id,
-        }
-    });
-
-    next();
-  }
-
   // home and connexion
     app.get('/', (req, res) => {
         (new Home()).print(req, res);
@@ -73,61 +40,60 @@ export default (app) => {
     });
 
   // admin
-    app.get('/admin', verifyJWT, (req, res) => {
+    app.get('/admin', (req, res) => {
         (new Dashboard()).print(req, res);
     });
 
   // admin/user
-    app.get('/admin/user', verifyJWT, (req, res) => {
+    app.get('/admin/user', (req, res) => {
         (new AdminUser()).print(req, res);
     });
 
-    app.get('/admin/user/delete/:id', verifyJWT, (req,res) => {
+    app.get('/admin/user/delete/:id', (req,res) => {
         (new AdminUser()).delete(req, res);
     });
 
-    app.get('/admin/user/edit/:id', verifyJWT, (req, res) => {
+    app.get('/admin/user/edit/:id', (req, res) => {
         (new AdminUser()).editForm(req, res);
     })
 
-    app.post('/admin/user/update/:id', verifyJWT, (req, res) => {
+    app.post('/admin/user/update/:id', (req, res) => {
         (new AdminUser()).editUser(req, res);
     })
 
   // admin/realty
-    app.get('/admin/realty', verifyJWT, (req, res) => {
+    app.get('/admin/realty', (req, res) => {
         (new AdminRealty()).print(req, res);
     })
 
-    app.get('/admin/realty/delete/:id', verifyJWT, (req, res) => {
+    app.get('/admin/realty/delete/:id', (req, res) => {
         (new AdminRealty()).delete(req, res);
     });
 
-    app.get('/admin/realty/create', verifyJWT, (req, res) => {
+    app.get('/admin/realty/create', (req, res) => {
         (new AdminRealty()).formNew(req, res);
     });
 
-    app.get('/admin/realty/show/:id', verifyJWT, (req,res) => {
+    app.get('/admin/realty/show/:id', (req,res) => {
         (new AdminRealty()).showRealty(req, res);
     });
 
     app.post('/admin/realty/new',
     expressFileupload({createParentPath: true}),
-    verifyJWT,
     (req, res) => {
         (new AdminRealty()).create(req, res);
     });
 
   //admin/contact
-    app.get('/admin/contact/byemail', verifyJWT, (req, res) => {
+    app.get('/admin/contact/byemail', (req, res) => {
         (new AdminContact()).ajaxContactByEmail(req, res);
     });
 
-    app.get('/admin/contact/search_partial_email', verifyJWT, (req, res) => {
+    app.get('/admin/contact/search_partial_email', (req, res) => {
         (new AdminContact()).ajaxSearchContactByEmail(req, res);
     });
 
-    app.get('/admin/contact', verifyJWT, (req, res) => {
+    app.get('/admin/contact', (req, res) => {
         (new AdminContact()).print(req, res);
     })
 };
