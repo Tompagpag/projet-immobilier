a49da8e32d793451680d763e2edfa607c4799a6f
Tom Paganel <paganel.tom@gmail.com>
2023-02-22 11:36:27 +0100

    add dockerfile for app and DB

--- test_bdd.js
+++ test_bdd.js
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-import connexion from "./app/database_sql.js"
+import connexion from "./web/www/app/database_sql.js"
 
 connexion.promise().query("SELECT 'connexion SQL OK'").then(([rows]) => {
     console.log(Object.values(rows[0]));
